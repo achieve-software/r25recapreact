140978d4222bd10645773c1f2a6acf9903e693fb
Achieve-software <vtcbcdtyr@gmail.com>
2023-07-22 14:05:47 +0300

    taxrate

--- src/components/CardTotal.jsx
+++ src/components/CardTotal.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const taxRate = 0.18;
+const taxRate = 0.2;
 const shipping = 25;
 
-const CardTotal = () => {
-  
+const CardTotal = ({products}) => {
+  const subTotal = products.reduce((acc,product)=> ((product.price) - (product.price* product.dampingRate))*  product.amount + acc, 0)
   
   
   return (
@@ -13,26 +13,26 @@ const CardTotal = () => {
         <tr className="text-end">
           <th className="text-start">Subtotal</th>
           <td>
-            $<span className="subtotal"></span>
+            $<span className="subtotal">{subTotal.toFixed(2)}</span>
           </td>
         </tr>
         <tr className="text-end">
-          <th className="text-start">Tax(18%)</th>
+          <th className="text-start">Tax(20%)</th>
           <td>
-            $<span className="tax"></span>
+            $<span className="tax">{(subTotal*taxRate).toFixed(2)}</span>
           </td>
         </tr>
         <tr className="text-end">
           <th className="text-start">Shipping</th>
           <td>
-            $<span className="shipping"></span>
+            $<span className="shipping">{shipping}</span>
           </td>
         </tr>
         <tr className="text-end">
           <th className="text-start">Total</th>
           <td>
             $
-            <span className="total">
+            <span className="total">{(subTotal + shipping + taxRate*subTotal).toFixed(2)}
             </span>
           </td>
         </tr>
