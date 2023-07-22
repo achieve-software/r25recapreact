1b9e9a585ecfbc90a68d42d77ee83f1f20452a3e
Achieve-software <vtcbcdtyr@gmail.com>
2023-07-22 12:40:40 +0300

    productlist

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,43 +1,56 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import ProductCard from "../components/ProductCard";
 import CardTotal from "../components/CardTotal";
-import axios from "axios";const ProductList = () => {  // const url = process.env.REACT_APP_API_URL;
-  const url = "https://64b8395f21b9aa6eb079b184.mockapi.io/products"  
-  console.log("url",url);  const [products, setProducts] = useState([])
-  const [loading, setLoading] = useState(true)
-  const [errorState, setErrorState] = useState(false)  
+import axios from "axios";
+const ProductList = () => {
+  // const url = process.env.REACT_APP_API_URL;
+  const url = "https://64b66bcedf0839c97e15895a.mockapi.io/products";
+  console.log("url", url);
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [errorState, setErrorState] = useState(false);
   const getProducts = async () => {
-    console.log("merhaba");    setLoading(false)
+    console.log("merhaba");
+    setLoading(false);
     try {
-      const { data } = await axios("")
-     setProducts(data)    
-      setErrorState(false)
+      const { data } = await axios(url);
+      setProducts(data);
+      setErrorState(false);
       console.log(data);
     } catch (error) {
-      console.log(error)
-      setErrorState(true)    }
-  }
+      console.log(error);
+      setErrorState(true);
+    }
+  };
   console.log(products);
   //? componentDidMount
   useEffect(() => {
-   getProducts()
-  }, [])  
+    getProducts();
+  }, []);
   return (
     <div className="container mt-3">
       <div className={"bg-light d-sm-block d-md-flex"}>
-       {loading ? <p className="text-center text-danger w-100">Loading....</p>
-        : products.length > 0 ?
-        <>
-          <article id="product-panel" className="col-md-5">
-            <ProductCard />
-          </article>
-          <article className="col-md-5 m-3">
-            <CardTotal />
-          </article>
-        </>
-         :
-        <p className="text-center text-danger w-100">No products data...</p> }        {errorState && ( <p className="text-center text-danger w-100">Error ...</p>)}
+        {loading ? (
+          <p className="text-center text-danger w-100">Loading....</p>
+        ) : products.length > 0 ? (
+          <>
+            <article id="product-panel" className="col-md-5">
+              <ProductCard />
+            </article>
+            <article className="col-md-5 m-3">
+              <CardTotal />
+            </article>
+          </>
+        ) : (
+          !errorState && (
+            <p className="text-center text-danger w-100">No products data...</p>
+          )
+        )}{" "}
+        {errorState && (
+          <p className="text-center text-danger w-100">Error ...</p>
+        )}
       </div>
     </div>
   );
-};export default ProductList;
+};
+export default ProductList;
