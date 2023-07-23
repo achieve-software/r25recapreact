10a9810c51e7b4c9df23018130e731c36c6eea50
Achieve-software <vtcbcdtyr@gmail.com>
2023-07-23 11:39:04 +0300

    end

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -1,35 +1,33 @@
-import React, {useState} from 'react'
-import ProductForm from "../components/ProductForm";
+import React, { useState } from 'react'
+import ProductForm from '../components/ProductForm';
 import axios from 'axios';
-
-
-const initialState ={
-  name:"",
+const initialState = {
+  name: "",
   image:"",
   price:0,
-  dampingRate:0,
+  dampingRate: 0.2,
   amount:1
 }
-
 const NewProduct = () => {
-  const url = "https://64b8395f21b9aa6eb079b184.mockapi.io/products";
-  const [formData, setFormData] = useState(initialState);
-
-  const handleChange =(e) => {
-    setFormData({...formData, [e.target.id]:e.target.value})
-  }
+  const url = "https://64b8395f21b9aa6eb079b184.mockapi.io/products"
+  const [formData, setFormData] = useState(initialState) 
+   const handleChange = (e)=>{
+    setFormData({...formData, [e.target.id]:e.target.value })//change eventının gerçekleştiği inputtaki id attribute u ile formDatamdaki key değerlerim aynı olduğu için dinamik bir şekilde formData mı güncelleybiliyorum
+    console.log({ [e.target.id]: e.target.value });
+  }  
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
        await axios.post(url, formData);
        setFormData(initialState)
-    } catch (error) {console.log(error)    }
+    } catch (error) {    }
   }
-  return (
+  console.log(initialState);  return (
     <div className='container'>
-      <ProductForm handleChange={handleChange} formData={formData} handleSubmit={handleSubmit}/> 
+      <ProductForm   handleChange={handleChange} formData={formData}
+      handleSubmit={handleSubmit}
+      text="New"/>
     </div>
   );
 }
-
 export default NewProduct
--- src/pages/UpdateProduct.jsx
+++ src/pages/UpdateProduct.jsx
@@ -1,33 +1,34 @@
-import React from 'react'
-import { useLocation } from "react-router-dom";
-import React, {useState} from "react"
 import axios from 'axios';
- import ProductForm from '../components/ProductForm';
-
-
+import React, { useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom';
+import ProductForm from '../components/ProductForm';
 const UpdateProduct = () => {
-  
-const {state:item} = useLocation();
+  const {state: item} = useLocation();
+// console.log(item);
+const url = "https://64b8395f21b9aa6eb079b184.mockapi.io/products"
 
-
-const url ="https://64b8395f21b9aa6eb079b184.mockapi.io/products"
 const [formData, setFormData] = useState(item)
-const handleChange =(e) => {
-  setFormData({...formData, [e.target.id]:e.target.value})
+const navigate = useNavigate()
+const handleChange = (e)=>{
+  setFormData({...formData, [e.target.id]:e.target.value })//change eventının gerçekleştiği inputtaki id attribute u ile formDatamdaki key değerlerim aynı olduğu için dinamik bir şekilde formData mı güncelleybiliyorum
+  console.log({ [e.target.id]: e.target.value });
 }
-
-const handleSubmit = async (e) => {
+const handleSubmit = async (e) =>{
   e.preventDefault();
   try {
-     await axios.post(url, formData);
-     setFormData(initialState)
-  } catch (error) {console.log(error)    }
-}
-  return (
-    <div className='container'>
-      <ProductForm formData={formData}/>
-    </div>
+    await axios.put(`${url}/${item.id}`, formData);
+    // setFormData(initialState)//post işleminden sonra formu boşaltmak için initialState değerini verdik
+ navigate(-1)
+  } catch (error) {
+    console.log(error)
+  }
+}  
+return (
+    <div className='container'>    
+      <ProductForm
+      handleChange={handleChange}
+      handleSubmit={handleSubmit}
+      formData={formData}/>    </div>
   );
 }
-
 export default UpdateProduct
